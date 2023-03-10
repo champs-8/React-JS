846dc03c18ebf0431559e1b68c793ad8ae6b1801
champs-8 <https://github.com/champs-8/Projetos>
2023-03-09 22:29:31 -0300

    modelagem no prisma

--- pizzaria/backend/src/routes.ts
+++ pizzaria/backend/src/routes.ts
@@ -3,12 +3,12 @@ import {Router, Request, Response} from 'express';
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
-    return res.json({name: "Minha picadura"});
+    // return res.json({name: "Minha picadura"});
 
 
     //exemplo de erro, exceção 
 
-    //  throw new Error('Erro ao fazer essa requisição');
+     throw new Error('Erro ao fazer essa requisição');
      
 });
 
